51880068bbcfcd07be22473a656fb5fe608f85b6
chris-macharia <christophermaina003@gmail.com>
2023-05-13 16:51:45 +0000

    Adjust the test for further test cases

--- models/base_model.py
+++ models/base_model.py
@@ -4,23 +4,23 @@ import uuid
 from datetime import datetime
 from . import storage
 
+
 class BaseModel:
     """BaseModel class"""
 
     def __init__(self, *args, **kwargs):
         """Initializes a new instance"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             dformat = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
-                if key != "__class__":
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.strptime(value, dformat))
+                elif key != "__class__":
                     setattr(self, key, value)
-            self.created_at = datetime.strptime(self.created_at, dformat)
-            self.updated_at = datetime.strptime(self.updated_at, dformat)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+        storage.new(self)
 
     def __str__(self):
         """Str representation of an instance"""
@@ -39,4 +39,3 @@ class BaseModel:
         dict["created_at"] = self.created_at.isoformat()
         dict["updated_at"] = self.updated_at.isoformat()
         return dict
-
--- test/test_base_model.py
+++ test/test_base_model.py
@@ -1,53 +1,43 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""...
-...
-...
-...
-"""
+"""Unittest for BaseModel class"""
+import unittest
 from models.base_model import BaseModel
 from datetime import datetime
-import pep8
-import unittest
 
 
 class TestBaseModel(unittest.TestCase):
-    """...
-    ...
-    ...
-    """
-    def test_pep8_of_base_model(self):
-        """...
-        ...
-        ...
-        """
-        syntax = pep8.StyleGuide(quit=True)
-        check = syntax.check_files(['models/base_model.py'])
-        self.assertEqual(
-            check.total_errors, 0,
-            "Found code style errors (and warnings)."
-        )
+    """Test cases for BaseModel class"""
 
-    def test_base_model_id_is_string(self):
-        """...
-        ...
-        ...
-        """
-        bm = BaseModel()
-        self.assertIsInstance(bm.id, str)
+    def setUp(self):
+        """Sets the class/obj"""
+        self.base_model = BaseModel()
+        self.base_model.id = "12345"
+        self.base_model.created_at = datetime.now()
+        self.base_model.updated_at = datetime.now()
 
-    def test_base_model_created_at_is_datetime(self):
-        """...
-        ...
-        ...
-        """
-        bm = BaseModel()
-        self.assertIsInstance(bm.created_at, datetime)
+    def test_save_method(self):
+        """Test case for 'save' method"""
+        datetime_prev = self.base_model.updated_at
+        self.base_model.save()
+        self.assertGreater(self.base_model.updated_at, datetime_prev)
 
-    def test_base_model_updated_at_is_datetime(self):
-        """...
-        ...
-        ...
-        """
-        bm = BaseModel()
-        self.assertIsInstance(bm.updated_at, datetime)
+    def test_str_method(self):
+        """Test case for str instance representation"""
+        cls_name = str(self.base_model.__class__.__name__)
+        obj_dict = str(self.base_model.__dict__)
+        obj_str = f"[{cls_name}] ({self.base_model.id}) {obj_dict}"
+        self.assertEqual(obj_str, self.base_model.__str__())
+
+    def test_to_dict_method(self):
+        """Test case for 'to_dict' method"""
+        dict = {
+            "id": self.base_model.id,
+            "__class__": self.base_model.__class__.__name__,
+            "created_at": self.base_model.created_at.isoformat(),
+            "updated_at": self.base_model.updated_at.isoformat()
+        }
+        self.assertDictEqual(dict, self.base_model.to_dict())
+
+
+if __name__ == "__main__":
+    unittest.main()
