48bc85f32b2aaf9ce596e8998e8b2c5671065cbb
chris-macharia <christophermaina003@gmail.com>
2023-05-14 01:19:36 +0000

    FEATURE: Enable user to create new instance of BaseModel, save it to the JSON and print its ID

--- console.py
+++ console.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Command interpreter to execute the hbnb console"""
 import cmd
+import shlex
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """Hbnb console"""
 
     prompt = "(hbnb) "
+    allowed_classes = {"BaseModel": BaseModel}
 
     def emptyline(self):
         """Method to cancel last command repetition"""
@@ -21,6 +25,19 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program\n"""
         print('Thank you for using Turtle')
         return True
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel.
+        """
+        command = self.parseline(line)[0]
+        if command is None:
+            print('** class name missing **')
+        elif command not in self.allowed_classes:
+            print("** class doesn't exist **")
+        else:
+            new_obj = eval(command)()
+            new_obj.save()
+            print(new_obj.id)
 
 
 if __name__ == '__main__':
